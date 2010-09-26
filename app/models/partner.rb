cda056539acdeb2da2259c42eec9cc3693813153
AJ Siegel <ritprez@gmail.com>
2010-09-26 16:58:35 -0400

    added gitignore file, partners css, and fixed adding new partners. added styling to tables for partners

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,9 +1,10 @@
 class Partner < ActiveRecord::Base
-
+  validates_format_of :website, :with => URI::regexp(%w(http https))
+  
   def photosets
 		output = []
 
-		json = Net::HTTP.get(URI.parse("http://api.flickr.com/services/rest/?method=flickr.collections.getTree&collection_id=72157624659199715&api_key=6f977fc52e81b8f45764d91ab84999c2&format=json&nojsoncallback=1&user_id=52768084@N06"))
+		json = Net::HTTP.get(URI.parse("http://api.flickr.com/services/rest/?method=flickr.collections.getTree&collection_id=72157625039715746&api_key=6f977fc52e81b8f45764d91ab84999c2&format=json&nojsoncallback=1&user_id=52768084@N06"))
     
 		result = ActiveSupport::JSON.decode(json)
 		unless result['stat'] == "fail"	
